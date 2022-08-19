ee89c725aabb6c0a8ac35b610b5b4ee1a0bcbb86
Marikuana <marians22092000@gmail.com>
2022-08-19 20:24:32 +0300

    Add Account Controller

---
+++ DotnetChat/Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+﻿using DotnetChat.Data;
+using DotnetChat.Data.Models;
+using DotnetChat.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace DotnetChat.Controllers
+{
+    public class AccountController : Controller
+    {
+        private ChatContext db;
+        public AccountController(ChatContext context)
+        {
+            db = context;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            User? user = await db.Users.FirstOrDefaultAsync(u => u.Login == model.Login && u.Password == model.Password);
+            if (user == null)
+            {
+                ModelState.AddModelError("", "Account not found");
+                return View(model);
+            }
+
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, user.Login),
+                new Claim("Id", user.Id.ToString())
+            };
+
+            ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", "Login", "Id");
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
+            return RedirectToAction("Index", "Chat");
+        }
+    }
+}
