197be8629093e3c4c83b0ed4451a20b731a1db15
Marikuana <marians22092000@gmail.com>
2022-08-19 20:27:27 +0300

    Add Account LoginView and LoginModel

---
+++ DotnetChat/Models/LoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotnetChat.Models
+{
+    public class LoginModel
+    {
+        [Required, MaxLength(64, ErrorMessage = "Max Lenght 64")]
+        public string Login { get; set; }
+
+        [Required, MaxLength(64, ErrorMessage = "Max Lenght 64")]
+        public string Password { get; set; }
+    }
+}
