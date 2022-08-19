d2930c78eb6f946e31ab604545f4eaa26c10cc51
Marikuana <marians22092000@gmail.com>
2022-08-19 20:25:34 +0300

    Add Initial Data to migration

--- DotnetChat/Data/ChatContext.cs
+++ DotnetChat/Data/ChatContext.cs
@@ -21,6 +21,37 @@ namespace DotnetChat.Data
 
             modelBuilder.Entity<Group>();
             modelBuilder.Entity<Private>();
+
+            InitData(modelBuilder);
         }
+
+        private void InitData(ModelBuilder modelBuilder)
+        {
+            User[] users = new User[]
+            {
+                new User { Id = 1, Login = "Marikuana", Password = "123" },
+                new User { Id = 2, Login = "Bob", Password = "123" },
+                new User { Id = 3, Login = "Tom", Password = "123" },
+            };
+
+            Group groupChat = new Group { Id = 1, Name = "Test Group" };
+            Private privateChat = new Private { Id = 2 };
+
+            modelBuilder.Entity<User>().HasData(users);
+            modelBuilder.Entity<Group>().HasData(groupChat);
+            modelBuilder.Entity<Private>().HasData(privateChat);
+
+            modelBuilder.Entity("ChatUser").HasData(new[]
+            {
+                new { ChatsId = 1, MembersId = 1 },
+                new { ChatsId = 1, MembersId = 2 },
+                new { ChatsId = 1, MembersId = 3 },
+
+                new { ChatsId = 2, MembersId = 1 },
+                new { ChatsId = 2, MembersId = 2 },
+            });
+        }
+
+        
     }
 }
---
+++ DotnetChat/Data/Migrations/20220819144557_InitData.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotnetChat.Migrations
+{
+    public partial class InitData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Chats",
+                columns: new[] { "Id", "Discriminator", "Name" },
+                values: new object[] { 1, "Group", "Test Group" });
+
+            migrationBuilder.InsertData(
+                table: "Chats",
+                columns: new[] { "Id", "Discriminator" },
+                values: new object[] { 2, "Private" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Login", "Password" },
+                values: new object[,]
+                {
+                    { 1, "Marikuana", "123" },
+                    { 2, "Bob", "123" },
+                    { 3, "Tom", "123" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ChatUser",
+                columns: new[] { "ChatsId", "MembersId" },
+                values: new object[,]
+                {
+                    { 1, 1 },
+                    { 1, 2 },
+                    { 1, 3 },
+                    { 2, 1 },
+                    { 2, 2 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "ChatUser",
+                keyColumns: new[] { "ChatsId", "MembersId" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "ChatUser",
+                keyColumns: new[] { "ChatsId", "MembersId" },
+                keyValues: new object[] { 1, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "ChatUser",
+                keyColumns: new[] { "ChatsId", "MembersId" },
+                keyValues: new object[] { 1, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "ChatUser",
+                keyColumns: new[] { "ChatsId", "MembersId" },
+                keyValues: new object[] { 2, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "ChatUser",
+                keyColumns: new[] { "ChatsId", "MembersId" },
+                keyValues: new object[] { 2, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "Chats",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Chats",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
