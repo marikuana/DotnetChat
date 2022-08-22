2361577b1ec87a09b7801887a2b90ec6dcd2ee4f
Marikuana <marians22092000@gmail.com>
2022-08-22 13:22:14 +0300

    update MessageViewModel

--- DotnetChat/Views/Models/MessageViewModel.cs
+++ DotnetChat/Views/Models/MessageViewModel.cs
@@ -5,5 +5,6 @@
         public int Id { get; set; }
         public UserViewModel Author { get; set; }
         public string Text { get; set; }
+        public DateTime CreatedDate { get; set; }
     }
 }
