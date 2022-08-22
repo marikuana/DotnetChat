8d88018890bbccfbaa652cfc6d165c35ce4ba234
Marikuana <marians22092000@gmail.com>
2022-08-22 13:22:37 +0300

    Add ChatHubExtensions

---
+++ DotnetChat/Extensions/ChatHubExtensions.cs
@@ -0,0 +1,13 @@
+﻿using DotnetChat.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace DotnetChat.Extensions
+{
+    public static class ChatHubExtensions
+    {
+        public static Task SendMessage(this IClientProxy clientProxy , MessageViewModel messageView)
+        {
+            return clientProxy.SendAsync("OnMessage", messageView);
+        }
+    }
+}
