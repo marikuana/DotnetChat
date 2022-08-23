69e8ef45f8bc71ee01c6ad971ec6a0a33fe2b194
Marikuana <marians22092000@gmail.com>
2022-08-23 21:04:55 +0300

    Add ChatController Chat Index View

---
+++ DotnetChat/Controllers/ChatController.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using DotnetChat.Data;
+using DotnetChat.Data.Models;
+using DotnetChat.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetChat.Controllers
+{
+    [Authorize]
+    public class ChatController : Controller
+    {
+        private IUserManager userManager;
+        private IMapper mapper;
+        private IUserService userService;
+        private IChatManager chatManager;
+
+        public ChatController(IUserManager userManager, IMapper mapper, IUserService userService, IChatManager chatManager)
+        {
+            this.userManager = userManager;
+            this.mapper = mapper;
+            this.userService = userService;
+            this.chatManager = chatManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            int id = userService.GetUserId(User);
+            User? user = userManager.GetUser(id);
+            if (user == null)
+                return NotFound();
+
+            var chats = userManager.GetUserChats(user);
+
+            foreach (var chat in chats) // REWORK
+            {
+                if (string.IsNullOrEmpty(chat.Name))
+                    chat.Name = chatManager.GetChatName(chat, user);
+            }
+
+            IEnumerable<ChatViewModel> chatsViews = mapper.Map<IEnumerable<Chat>, IEnumerable<ChatViewModel>>(chats);
+            return View(chatsViews);
+        }
+    }
+}
