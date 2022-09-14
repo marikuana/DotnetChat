20f13cae0c6566de9a9bae1400fae26ef538ae55
Marikuana <marians22092000@gmail.com>
2022-09-14 15:51:50 +0300

    Create UserSenderService

---
+++ DotnetChat/Interfaces/IUserSenderService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace DotnetChat.Controllers
+{
+    public interface IUserSenderService
+    {
+        void Send(IEnumerable<int> userIds, Action<IClientProxy> action);
+    }
+}
